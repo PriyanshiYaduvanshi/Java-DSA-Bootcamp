38d8bdc5cb8321ade1fe8ed045c7d093abaca6a7
Priyanshi Yaduvanshi <154300500+PriyanshiYaduvanshi@users.noreply.github.com>
2026-08-08 22:32:20 +0530

    Modify return value for target match in floor function
    Return the value at mid instead of the index when the target is found.

--- Searching Algorithm/3. Floor of a number/floor.java
+++ Searching Algorithm/3. Floor of a number/floor.java
@@ -14,7 +14,7 @@ public class Floor {
         while(left <= right){
             int mid = left +(right-left)/2;
             if(arr[mid] == target)
-                return mid;
+                return arr[mid];
             else if(arr[mid]<target){
                 left = mid+1;
             }
